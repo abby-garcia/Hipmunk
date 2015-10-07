03a7ab6df85bdba78c7419748a8b65e86d50da59
abby-garcia <acg361@gmail.com>
2015-10-07 11:16:45 -0400

    airports

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
-$(function() {
-    $( "#from" ).datepicker({
-      defaultDate: "+1w",
-      changeMonth: true,
-      numberOfMonths: 3,
-      onClose: function( selectedDate ) {
-        $( "#to" ).datepicker( "option", "minDate", selectedDate );
-      }
+$( document ).ready(function() {
+    $(function() {
+        $( ".autocomplete" ).autocomplete({
+            source: function( request, response ) {
+                $.ajax({
+                    url: "http://www.air-port-codes.com/search/",
+                    jsonp: "callback",
+                    dataType: "jsonp",
+                    data: {
+                    term: request.term, // input field value
+                    limit: 7, // default is 30
+                    size: 3, // default is 0
+                    key: "[API-KEY-HERE]" // dont forget to add your API Key from your air-port-codes account
+                },
+                success: function( data ) {
+                    if (data.status) { // success
+                        response( $.map( data.airports, function( item ) {
+                            return {
+                                label: item.name + ' (' + item.iata + ')',
+                                value: item.name + ' (' + item.iata + ')',
+                                code: item.iata
+                            }
+                        }));
+                    } else { // no results
+                        response();
+                    }
+                }
+            });
+},
+select: function( event, ui ) {
+            // do something for click event
+            console.log(ui.item.code);
+        }
     });
-    $( "#to" ).datepicker({
-      defaultDate: "+1w",
-      changeMonth: true,
-      numberOfMonths: 3,
-      onClose: function( selectedDate ) {
-        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
-      }
-    });
-  });
+});
+});
